2e9835cd20ae192e6ed78c62965c6b3346ade5a1
igneel73 <igneel.kush73@gmail.com>
2021-11-13 03:46:59 +0530

    add arg parser, and validations

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 from flask import Flask
-from flask_restful import Api, Resource
-from database import init_db
+from flask_restful import Api, Resource, reqparse, abort
+from database import init_db, db_session
+from models import Asset, Asset_Account
 
 
 # setup
 app = Flask(__name__)
 api = Api(app)
 init_db()
+
+# argument parsing, and validations
+parser = reqparse.RequestParser()
+parser.add_argument('asset_type')
+parser.add_argument('deposit_amt', type=float)
+parser.add_argument('withdrawal_amt', type=float)
+parser.add_argument('src_acc_no', type=int)
+parser.add_argument('dest_acc_no', type=int)
+parser.add_argument('src_asset_type')
+parser.add_argument('dest_asset_type')
+parser.add_argument('transfer_amt', type=float)
+
+
+def abort_if_insufficient_bal(acc_no, asset_type, amt):
+    asset = Asset.query.filter(
+        Asset.owner_account == acc_no, Asset.asset == asset_type).first()
+    if asset == None or asset.amount < amt:
+        abort(401, message=f'account {acc_no} has insufficient balance')
+
+
+def abort_if_invalid_account(acc_no):
+    account = db_session.get(Asset_Account, acc_no)
+    if account == None:
+        abort(401, message=f'account {acc_no} does not exist')
+
 
 # Resource
 
